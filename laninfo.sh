e4d0a6ed1ed0395c6fa08aaef7e5bf00e59314f0
Dexter H. Hu <hyhu@cs.hku.hk>
2011-08-19 15:48:50 +0800

    modified:   laninfo.sh

--- laninfo.sh
+++ laninfo.sh
@@ -17,7 +17,12 @@
 #       CREATED:  04/12/2010 04:24:02 PM HKT
 #      REVISION:  ---
 #===============================================================================
+if [ $# -ne 1 ]
+then
+echo "Usage: $(basename $0) ethX"
+exit 1
+fi
 
-sudo arp-scan -l | grep -Eo '\b[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\b' | awk '{print $1}' | xargs -I {} dig +short -x {}
+sudo arp-scan --interface $1 -l | grep -Eo '\b[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\b' | awk '{print $1}' | xargs -I {} -i bash -c 'echo -n "{} " ; dig +short -x {}'
 
 
