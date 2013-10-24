65f3c6ce1629d0de8593ec0175e1850e81331b17
test test <dexterhu@ubuntu.(none)>
2013-10-24 12:37:23 +0800

    install chrome on ubuntu

--- qa-setup.sh
+++ qa-setup.sh
@@ -8,3 +8,11 @@ sudo apt-get update
 sudo apt-get -y install jenkins curl python maven subversion git xvfb htop maven
 sudo apt-get -y install oracle-java7-installer
 sudo update-java-alternatives -s java-7-oracle
+
+# Ubuntu change dash to bash
+sudo dpkg-reconfigure dash
+
+# Ubuntu Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -p /tmp
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+sudo apt-get -f install
