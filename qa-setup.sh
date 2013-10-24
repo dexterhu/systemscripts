9440d5bbd84c723aab8ced1479235d3ce3d49638
test test <dexterhu@ubuntu.(none)>
2013-10-24 11:43:56 +0800

    add oracle install

--- qa-setup.sh
+++ qa-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sudo locale-gen en_US.UTF-8
+locale -a
 sudo apt-get -y install software-properties-common
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo apt-get update
 sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
 sudo apt-get -y install jenkins curl python maven subversion git xvfb htop maven
 sudo apt-get -y install oracle-java7-installer
-
+sudo update-java-alternatives -s java-7-oracle
